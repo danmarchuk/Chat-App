2a7e38403a91e4c9048e07f09d51122e3c220db0
Danyil Marchuk <zemero58@gmail.com>
2023-01-05 01:20:18 +0100

    Adding Firebase and updating the functionality of the Register window

--- Chat App/Controllers/RegisterViewController.swift
+++ Chat App/Controllers/RegisterViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import FirebaseAuth
+import SwiftUI
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    let errorString = String(e.localizedDescription)
+                    self.showSimpleAlert(title: "Error", message: errorString)
+                } else {
+                    // Navigate the user to the ChatViewController
+                    self.performSegue(withIdentifier: "registerToChat", sender: self)
+                }
+            }
+        }
     }
     
+    func showSimpleAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: { _ in
+                //Cancel Action
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    
 }
+
+
